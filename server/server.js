50bc994729a829153c4e780e0a639a6ee4a5d0d4
Akash <akashkarwande@outlook.com>
2020-08-09 21:01:56 +0530

    emit message to all connected users

--- server/server.js
+++ server/server.js
@@ -18,15 +18,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-
-    socket.emit('newMessage', {
-        from: "akash@gmail.com",
-        text: "testing",
-        createdAt: 123
-    });
-
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
