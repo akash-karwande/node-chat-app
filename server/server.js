478b010dccccf446a4dc937de8d4dac128304393
Akash <akashkarwande@outlook.com>
2020-08-09 19:59:14 +0530

    connected to socket io

--- server/server.js
+++ server/server.js
@@ -1,18 +1,29 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
 
 
 const publicPath = path.join(__dirname, '../public');
 var port = process.env.PORT || 3000;
 var app = express();
+
+var server = http.createServer(app);
+
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.get('/', (req, res) => {
-    res.send('hey there');
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user is disconnected');
+    });
 });
 
 
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log(`started up at port ${port}`);
 });
