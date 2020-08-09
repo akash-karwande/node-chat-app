99a87d8f09df370d3be70a27bcf5674e9722cf68
Akash <akashkarwande@outlook.com>
2020-08-09 21:16:37 +0530

    add welcome message

--- server/server.js
+++ server/server.js
@@ -18,6 +18,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: "Admin",
+        text: "Welcome to app",
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: "Admin",
+        text: "New user joined",
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
 
